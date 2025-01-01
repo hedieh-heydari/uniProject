9fb55a5ed835ead99966ec371659deb85f1ddb14
hedieh <heydari.hedieh2680@gmail.com>
2025-01-01 11:48:22 +0330

    delete extra fragments

--- src/assets/icons/PlusIcon.tsx
+++ src/assets/icons/PlusIcon.tsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { iconInterface } from "./IconInterface";
+import React from 'react';
+import { iconInterface } from './IconInterface';
 
 const PlusIcon: React.FC<iconInterface> = ({ className }) => {
-  return (
-    <>
+   return (
       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 16 16"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className={`${className}`}
+         xmlns="http://www.w3.org/2000/svg"
+         fill="none"
+         viewBox="0 0 16 16"
+         strokeWidth="1.5"
+         stroke="currentColor"
+         className={`${className}`}
       >
-        <path
-          d="M2.66699 7.99996H13.3337M8.00033 2.66663V13.3333"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
+         <path
+            d="M2.66699 7.99996H13.3337M8.00033 2.66663V13.3333"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
       </svg>
-    </>
-  );
+   );
 };
 export default PlusIcon;
