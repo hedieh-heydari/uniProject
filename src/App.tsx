84c0d88039bda56cf671cbcb8d5b3dc1bc1b8865
Hedieh <heydari.hedieh2680@gmail.com>
2025-01-30 19:45:13 +0330

    check if token is null refirect to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import MainLayout from './feature/Layout/MainLayout';
+import { useNavigate } from 'react-router-dom';
+
 
 function App() {
+   const navigate = useNavigate()
+   console.log(localStorage.getItem('token'))
+   useEffect(() => {
+      if (!localStorage.getItem('token'))
+         navigate('/login')
+   },[])
+
+
    return <MainLayout />;
 }
 
