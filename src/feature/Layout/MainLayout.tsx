0082f83602bf2182711ae53442bbdbe43d17e7ef
Hedieh <heydari.hedieh2680@gmail.com>
2025-01-30 21:24:30 +0330

    fix showing toast in login page

--- src/feature/Layout/MainLayout.tsx
+++ src/feature/Layout/MainLayout.tsx
@@ -2,25 +2,11 @@ import { Suspense, useState } from 'react';
 import { NextUIProvider } from '@nextui-org/react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
-import { ToastContainer, Bounce } from 'react-toastify';
 
 const MainLayout = () => {
    const [open, setOpen] = useState<boolean>(false);
    return (
       <body className="font-IranYekanNumber">
-         <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={true}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            transition={Bounce}
-         />
          <Sidebar open={open} setOpen={setOpen} />
          <div
             className={`min-h-screen h-full w-full overflow-hidden transition-all duration-300 ${
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,23 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
+import { Bounce, ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <RouterProvider router={router} />
+      <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={true}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition={Bounce}
+         />
    </StrictMode>
 );
