98c9bb4e7fbaeb932dd313682e55d96b0555fe6d
Hedieh <heydari.hedieh2680@gmail.com>
2025-02-02 18:03:52 +0330

    delete extra comments

--- src/feature/category/views/BoxesCategory.tsx
+++ src/feature/category/views/BoxesCategory.tsx
@@ -23,14 +23,6 @@ const BoxesCategory = () => {
                setOpenUpsertModal(true);
             }}
          />
-         {/* <CardsComponent
-            categoryName="test"
-            icon={<WalletExportIcon className="w-5 h-5" />}
-            editFunction={() => {
-               setOpenUpsertModal(true)
-            }}
-            amount=" amount: 12345"
-         /> */}
          {boxesData &&
             boxesData.map((i: IIncomeList, index: number) => {
                return (
