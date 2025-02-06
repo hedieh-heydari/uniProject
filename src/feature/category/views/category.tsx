d34f100d56d63fd3fa5033ddbf5bf587385328b9
Hedieh <heydari.hedieh2680@gmail.com>
2025-02-06 22:14:44 +0330

    add search parameter for category page

--- src/feature/category/views/category.tsx
+++ src/feature/category/views/category.tsx
@@ -3,10 +3,40 @@ import CardExchangeIcon from '../../../assets/icons/CardExchangeIcon';
 import IncomeCategory from './IncomeCategory';
 import OutgoCategory from './OutgoCategory';
 import BoxesCategory from './BoxesCategory';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const CategoryIndex = () => {
-   const [selectedTab, setSelectedTab] = useState<string>('import');
+   const navigate = useNavigate();
+   const location = useLocation();
+
+   console.log(location);
+   const page = location.search.split('=')[1];
+   const [selectedTab, setSelectedTab] = useState<string>(page);
+
+   useEffect(() => {
+      if (page) {
+         setSelectedTab(page);
+      } else {
+         setSelectedTab('import');
+      }
+   }, [page]);
+
+   const tabData = [
+      {
+         key: 'import',
+         title: 'ورودی'
+      },
+      {
+         key: 'export',
+         title: 'خروجی'
+      },
+      {
+         key: 'boxes',
+         title: 'صندوق‌ها'
+      }
+   ];
+
    return (
       <>
          <Card className="px-4 py-6 my-6 overflow-visible bg-gradient-to-r from-green-50 to-[#ddece7] shadow-lg rounded-xl">
@@ -27,17 +57,19 @@ const CategoryIndex = () => {
                      classNames={{
                         panel: '!px-0',
                         tabList: 'bg-green-800 text-white w-full',
-                        tabContent: 'text-white group-data-[selected=true]:text-black ',
+                        tabContent:
+                           'text-white group-data-[selected=true]:text-black ',
                         cursor: '!bg-green-100',
-                        base: 'overflow-auto lg:w-4/12 ',
+                        base: 'overflow-auto lg:w-4/12 '
                      }}
+                     selectedKey={selectedTab}
                      onSelectionChange={(e: any) => {
-                        setSelectedTab(e);
+                        navigate(`/category?page=${e}`);
                      }}
                   >
-                     <Tab key="import" title="ورودی"></Tab>
-                     <Tab key="export" title="خروجی"></Tab>
-                     <Tab key="boxes" title="صندوق‌ها"></Tab>
+                     {tabData.map((i: any) => {
+                        return <Tab key={i.key} title={i.title}></Tab>;
+                     })}
                   </Tabs>
                </div>
                {selectedTab === 'import' ? (
